143560f4f8a264dafcd56f5a247db772c761263c
Ziad Sayed <ziadsayed99@gmail.com>
2024-11-04 18:41:05 +0200

    adding meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,25 @@
-export default function MealsPage() {
-  return <h1>Meals</h1>;
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals.grid";
+import { getMeals } from "@/lib/meals";
+
+export default async function MealsPage() {
+  const meals = await getMeals();
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicous meals, created{" "}
+          <span className={classes.highlight}> by you </span>
+        </h1>
+        <p>Find your favorite meals, and share them with the world!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe </Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
+  );
 }
