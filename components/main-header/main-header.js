11a17e7b965e564f7c570c4291abe3dfb1f5fba6
Ziad Sayed <ziadsayed99@gmail.com>
2024-11-04 18:00:56 +0200

    finishing community page

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -3,6 +3,7 @@ import logo from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
   return (
@@ -18,10 +19,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Explore Recipes</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Community</NavLink>
             </li>
           </ul>
         </nav>
