075c65184b4ae04dcec3a1ae14fb201c49c894b4
Ziad Sayed <ziadsayed99@gmail.com>
2025-01-09 09:33:15 +0200

    error handle + loading module

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Unfortunately, the page you are looking for was not found.</p>
+    </main>
+  );
+}
